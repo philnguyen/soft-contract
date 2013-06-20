c25f57a4f9bdc4a1101369ad9f93ba3ef437200a
Phil Nguyen <pnguyen@ccs.neu.edu>
2013-06-20 18:11:29 -0400

    print concise test info

--- test/run-all.rkt
+++ test/run-all.rkt
@@ -7,12 +7,16 @@
          [(vector) (sort (map path->string (directory-list)) string<=?)]
          [names names])]
       #:when (regexp-match? #rx"sch$" filename))
-  (let ([lines# (with-input-from-file filename
-                  (thunk (length (port->lines (current-input-port)))))])
+  (let ([lines (with-input-from-file filename
+                  (thunk (length (port->lines (current-input-port)))))]
+        [name (string-trim filename ".sch")])
     (with-input-from-file filename
       (thunk
        (let ([prog (for/list ([form (in-port read)]) form)])
-         (printf "~a: ~a lines~n" filename lines#)
-         (let ([ans (time (ev prog))])
-           (for ([a ans]) (printf "-- ~a~n" a)))
-         (printf "~n"))))))
+         #;(begin ; verbose
+           (printf "~a: ~a lines~n" name lines)
+           (for ([a (time (ev prog))]) (printf "-- ~a~n" a))
+           (printf "~n"))
+         (begin ; concise
+           (let-values ([(_r time _1 _2) (time-apply ev (list prog))])
+             (printf "~a,~a,~a~n" name lines time))))))))
