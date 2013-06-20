618e73e037555454568c2cca35bfa913a0cddf2a
Phil Nguyen <pnguyen@ccs.neu.edu>
2013-06-20 18:38:29 -0400

    change binary-tree example

--- test/run-all.rkt
+++ test/run-all.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require "../machine.rkt")
 
+(define verbose? #f)
+
 (for ([filename
        ; commandline parameters as files to run. Run all by default.
        (match (current-command-line-arguments)
@@ -8,15 +10,15 @@
          [names names])]
       #:when (regexp-match? #rx"sch$" filename))
   (let ([lines (with-input-from-file filename
-                  (thunk (length (port->lines (current-input-port)))))]
+                 (thunk (length (port->lines (current-input-port)))))]
         [name (string-trim filename ".sch")])
     (with-input-from-file filename
       (thunk
        (let ([prog (for/list ([form (in-port read)]) form)])
-         #;(begin ; verbose
-           (printf "~a: ~a lines~n" name lines)
-           (for ([a (time (ev prog))]) (printf "-- ~a~n" a))
-           (printf "~n"))
-         (begin ; concise
-           (let-values ([(_r time _1 _2) (time-apply ev (list prog))])
-             (printf "~a,~a,~a~n" name lines time))))))))
+         (if verbose?
+             (begin
+               (printf "~a: ~a lines~n" name lines)
+               (for ([a (time (ev prog))]) (printf "-- ~a~n" a))
+               (printf "~n"))
+             (let-values ([(_r time _1 _2) (time-apply ev (list prog))])
+               (printf "~a & ~a & ~a \\\\ ~n" name lines time))))))))
