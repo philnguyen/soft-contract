44ec6c466ce8acb14407a1da0283f5389e7e3942
Phil Nguyen <pnguyen@ccs.neu.edu>
2013-06-30 17:37:37 -0400

    update run-all script too print results for both interpreters

--- test/run-all.rkt
+++ test/run-all.rkt
@@ -1,5 +1,7 @@
 #lang racket
-(require "../machine.rkt" "../syntax.rkt")
+(require (only-in "../machine.rkt" ev)
+         (only-in "../machine-dumb.rkt" [ev ev1])
+         "../syntax.rkt")
 
 (define verbose? #f)
 (define files '())
@@ -11,24 +13,32 @@
      (set! files (sort (map path->string (directory-list)) string<=?))
      (set! files fnames)))
 
-(define (exec prog)
+(define (exec ev prog)
   (let-values ([(r t1 t2 t3) (time-apply ev (list prog))])
     (list (first r) t1 t2 t3)))
 
-(for ([filename files]
-      #:when (regexp-match? #rx"sch$" filename))
+(define/match (a->time ans)
+  [(#f) "-"]
+  [((list (list _ t _ _))) t])
+
+(define/match (a->blames ans)
+  [(#f) "-"]
+  [((list (list as _ _ _))) (for/sum ([a as] #:when (match? a `(blame ,_ ,_))) 1)])
+
+(for ([filename files] #:when (regexp-match? #rx"sch$" filename))
   (let ([lines (length (file->lines filename))]
         [name (string-trim filename ".sch")]
         [prog (file->list filename)])
-    (match (within-time 60 (exec prog))
-      [#f (if verbose? (printf "~a: ~a lines, timeout~n~n" name lines)
-              (printf "~a & ~a & - & -\\\\~n" name lines))]
-      [(list (list r t1 t2 t3))
-       (if verbose?
-           (begin
-             (printf "~a: ~a lines~ncpu time: ~a, real time: ~a, gc time: ~a~n"
-                     name lines t1 t2 t3)
-             (for ([a (in-set r)]) (printf "-- ~a~n" a))
-             (printf "~n"))
-           (let ([blames (for/sum ([ri r] #:when (match? ri `(blame ,_ ,_))) 1)])
-             (printf "~a & ~a & ~a & ~a\\\\~n" name lines t1 blames)))])))
+    (if verbose?
+        ; only run the 'normal' evaluator for verbose mode
+        (match (within-time 60 (exec ev prog))
+          [#f (printf "~a: ~a lines, timeout~n~n" name lines)]
+          [(list (list r t1 t2 t3))
+           (printf "~a: ~a lines~ncpu time: ~a, real time: ~a, gc time: ~a~n" name lines t1 t2 t3)
+           (for ([a (in-set r)]) (printf "-- ~a~n" a))
+           (printf "~n")])
+        ; compare with dumb-ized interpreter, dump table in latex format
+        (let ([a1 (within-time 60 (exec ev prog))]
+              [a2 (within-time 60 (exec ev1 prog))])
+          (printf "~a & ~a & ~a / ~a & ~a / ~a\\\\~n"
+                  name lines (a->time a1) (a->time a2) (a->blames a1) (a->blames a2))))))
