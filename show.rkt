727b482740da4c5d3daa7a6da24bb9bc5daddc5f
Phil Nguyen <pnguyen@ccs.neu.edu>
2013-07-07 01:26:19 -0400

    implement values/contracts (semi-)pretty-printing

---
+++ show.rkt
@@ -0,0 +1,81 @@
+#lang racket
+(require
+ "lang.rkt"
+ (only-in redex variables-not-in))
+
+(define vars-not-in
+  (let* ([pool '(x y z u v w a b c)]
+         [N (length pool)])
+    (λ (n t)
+      (reverse ; just for nice order
+       (variables-not-in t (if (<= n N) (take pool n) (make-list n 'x1)))))))
+
+(provide
+ #;(all-defined-out)
+ (contract-out
+  [show-E (E? . -> . any)]
+  [show-U (U? . -> . any)]
+  [show-C (C? . -> . any)]
+  [show-c (c? . -> . any)]
+  [show-e (e? . -> . any)]
+  [show-b (b? . -> . any)]))
+
+(define/match (show-E E)
+  [((close e _)) (show-e e)]
+  [((val U Cs)) (match U
+                  [(•) (cons '• (for/list ([C Cs]) (show-C C)))]
+                  [_ (show-U U)])]
+  [((Blm l+ lo s)) `(Blame ,l+ ,lo ,s)]
+  [((? L? l)) `(L ,l)]
+  [((Mon _ _ _ C E)) `(MON ,(show-C C) ,(show-E E))]
+  [((FC _ C V)) `(FC ,(show-C C) ,(show-E V))]
+  [((Assume V C)) `(ASSUME ,(show-E V) ,(show-C C))])
+
+(define/match (show-U U)
+  [((close f _)) (show-e f)]
+  [((•)) `•]
+  [((Struct t Vs)) (match (length Vs)
+                     [0 t]
+                     [_ `(,t ,@ (map show-E Vs))])]
+  [((Arr _ _ _ C V)) `(=> ,(show-C C) ,(show-E V))]
+  [((? b? b)) (show-b b)])
+
+(define/match (show-C C)
+  [((close c _)) (show-c c)])
+
+(define/match (show-c c)
+  [((func-c xs y v?)) `(,@ (map show-c xs) ,(if v? '↦* '↦) ,(show-c y))]
+  [((and-c c1 c2)) `(and/c ,(show-c c1) ,(show-c c2))]
+  [((or-c c1 c2)) `(or/c ,(show-c c1) ,(show-c c2))]
+  [((struct-c t cs))
+   `(,(string->symbol (string-append (symbol->string t) "/c")) ,@ (map show-c cs))]
+  [((μ-c x c)) `(μ ,x ,(show-c c))]
+  [((? v? v)) (show-e v)])
+
+(define (show-e e [ctx '()])
+  (match e
+    [(f n e _) (let ([xs (vars-not-in n ctx)])
+                 `(λ ,xs ,(show-e e (append xs ctx))))]
+    [(•) '•]
+    [(x sd) (if (< sd (length ctx)) (list-ref ctx sd) '⋯)]
+    [(ref _ _ x) x]
+    [(@ _ (f 1 (if/ (x 0) (x 0) e2) #f) (list e1))
+     `(,(show-e e1 ctx) ∨ ,(show-e e2 ctx))]
+    [(@ _ (f n ey #f) (list ex ...))
+     (let ([xs (vars-not-in n ctx)])
+       `(let ,(for/list ([x (reverse xs)] [e ex]) `[,x ,(show-e e ctx)])
+          ,(show-e ey (append xs ctx))))]
+    [(@ _ f xs) `(,(show-e f ctx) ,@ (for/list ([x xs]) (show-e x ctx)))]
+    [(if/ e1 e2 #t) `(,(show-e e1 ctx) ⇒ ,(show-e e2 ctx))]
+    [(if/ e1 e2 #f) `(,(show-e e1 ctx) ∧ ,(show-e e2 ctx))]
+    [(if/ e1 e2 e3) `(if ,(show-e e1 ctx) ,(show-e e2 ctx) ,(show-e e3 ctx))]
+    [(amb _) 'amb]
+    [(? b? b) (show-b b)]))
+
+(define/match (show-b b)
+  [((op name)) (match name ['false? '¬] ['<= '≤] ['>= '≥] [_ name])]
+  [((struct-mk t _)) t]
+  [((struct-p t _)) (string->symbol (string-append (symbol->string t) "?"))]
+  [((struct-ac t n i)) (string->symbol (string-append (symbol->string t) "-" (number->string i)))]
+  [((? string? s)) (string-append "\"" s "\"")]
+  [(b) b])
