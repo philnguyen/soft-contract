8bd6ed5814c9bd95b459729016e63e7b75437484
Phil Nguyen <pcn@cs.umd.edu>
2014-12-05 10:32:29 -0500

    fix error message

--- check.rkt
+++ check.rkt
@@ -4,30 +4,33 @@
          (only-in "utils.rkt" match?)
          (only-in "lang.rkt" .p)
          (only-in "verify/machine.rkt" .ς [e verify])
-         (only-in "verify/closure.rkt" .blm?)
+         (prefix-in ve: (only-in "verify/closure.rkt" .blm? .blm .σ .σ?))
+         (prefix-in ve: (only-in "verify/show.rkt" show-V))
          (only-in "terify/machine.rkt" [ev find-error])
          (only-in "terify/provability.rkt" [model model/untyped])
          (only-in "terify/query-z3.rkt" [model model/z3])
-         (only-in "terify/show.rkt" show-ce show-A)
-         (only-in "terify/closure.rkt" .σ?))
+         (prefix-in ce: (only-in "terify/show.rkt" show-ce show-A show-V))
+         (prefix-in ce: (only-in "terify/closure.rkt" .blm? .blm .σ .σ?)))
 (require/typed "terify/read.rkt"
   [(read-p read/ce) (Sexp → .p)])
-
-(define-type Result (U 'timeout Ce-Result Ve-Result))
-(define-type Ce-Result (U 'safe (List 'ce (Option (List Any Any)))))
-(define-type Ve-Result (U 'safe (List 'error Any)))
 
 (: feedback ([Sexp] [Integer] . ->* . Any))
 (define (feedback prog [timeout 30])
   (match (run prog timeout)
     ['timeout (printf "Timeout after ~a seconds~n" timeout)]
-    ['safe (printf "Program is safe")]
-    [`(error (blame ,who ,whom ,what ,why))
-     (raise-contract-error who whom what why)]
-    [`(ce #f) ; FIXME
-     (error "Contract violation")]
-    [`(ce ((blame ,who ,whom ,what ,why) ,ce))
-     (raise-contract-error who whom what why ce)]))
+    [(or 'safe (list)) (printf "Program is safe")]
+    [(list 'blame l⁺ lᵒ v c)
+     (raise-contract-error l⁺ lᵒ v c)]
+    [(list 'ce (list 'blame l⁺ lᵒ v c) ce)
+     (raise-contract-error l⁺ lᵒ v c ce)]
+    [(cons (list 'blame l⁺ lᵒ v c) _)
+     ;; Raise first error
+     (raise-contract-error l⁺ lᵒ v c)]))
+
+(define-type Result (U 'timeout Ce-Result Ve-Result))
+(define-type Ce-Result (U 'safe Err-Result (List 'ce Err-Result Any)))
+(define-type Ve-Result (Listof Err-Result))
+(define-type Err-Result (List 'blame Symbol Symbol Any Any))
 
 (: run : Sexp Integer → Result)
 ;; Verify + Seek counterexamples at the same time, whichever finishes first
@@ -41,7 +44,7 @@
     (kill-thread ce-thread)
     (kill-thread timeout-thread))
   (match (channel-get c)
-    [(and err (list 'error Any))
+    [(and err (or (cons 'blame _) (cons (cons 'blame _) _)))
      ;; Wait for (maybe) counterexample, unless timeout
      (match (channel-get c)
        ['timeout (kill-all) err]
@@ -52,12 +55,9 @@
 ;; Run verification on program
 (define (try-verify prog)
   (define ςs (verify prog))
-  (define errors
-    (for/list : (Listof Any) ([ς ςs] #:when (match? ς (.ς (? .blm?) _ _)))
-      ς))
-  (match errors
-    [(list) #;(printf "VE says safe~n") 'safe]
-    [errors (list 'error (remove-duplicates errors))]))
+  (for/list : (Listof Err-Result) ([ς ςs] #:when (match? ς (.ς (? ve:.blm?) _ _)))
+    (match-define (.ς (ve:.blm l⁺ lᵒ v c) σ _) ς)
+    (list 'blame l⁺ lᵒ (ve:show-V σ v) (ve:show-V σ c))))
 
 (: try-find-ce : Sexp → Ce-Result)
 ;; Run counterexample on program
@@ -66,40 +66,26 @@
   #;(printf "CE read~n")
   (match (find-error p)
     [#f #;(printf "CE says safe~n") 'safe]
-    [(cons σ^ blm)
+    [(cons σ^ (ce:.blm l⁺ lᵒ v c))
      #;(printf "CE says CE~n")
-     (list 'ce
-           (match (model/z3 (model/untyped p σ^))
-             [#f #;(printf "CE can't find~n") #f]
-             [(? .σ? σ) #;(printf "CE finds~n") (list (show-A σ blm) (show-ce p σ))]))]))
+     (match (model/z3 (model/untyped p σ^))
+       [#f (list 'blame l⁺ lᵒ (ce:show-V σ^ v) (ce:show-V σ^ c))]
+       [(? ce:.σ? σ) (list 'ce (list 'blame l⁺ lᵒ (ce:show-V σ v) (ce:show-V σ c))
+                           (second #|for now|# (ce:show-ce p σ)))])]))
 
 (: raise-contract-error ([Any Any Any Any] [Any] . ->* . Any))
-(define (raise-contract-error who whom what why [ce #f])
-  ;; FIXME shameless code dup
-  (cond
-   [(equal? who whom)
-    (cond
-     [ce
-      (error
-       '|Contract violation|
-       "'~a' violates its own contract:~n Value:~n  ~a~n violates predicate~n  ~a~nAn example program that breaks it:~n ~a"
-       who (pretty what) (pretty why) (pretty ce))]
-     [else
-      (error '|Contract violation|
-             "'~a' violates its own contract:~n Value:~n  ~a~n violates predicate~n  ~a."
-             who (pretty what) (pretty why))])]
-   [else
-    (cond
-     [ce
-      (error
-       '|Contract violation|
-       "'~a' violates '~a':~n Value:~n  ~a~n violates predicate~n  ~a~nAn example program that breaks it:~n ~a"
-       who whom (pretty what) (pretty why) (pretty ce))]
-     [else
-      (error
-       '|Contract violation|
-       "'~a' violates '~a':~n Value:~n  ~a~n violates predicate~n  ~a."
-       who whom (pretty what) (pretty why))])]))
+(define (raise-contract-error l⁺ lᵒ v c [ce #f])
+  (define parties
+    (cond [(equal? l⁺ lᵒ) (format "'~a' violates its own contract." l⁺)]
+          [else (format "'~a' violates '~a'." l⁺ lᵒ)]))
+  (define reason
+    (match* (v c)
+      [(_ `(arity=/c ,_)) "Wrong arity"]
+      [(_ _) (format "Value~n ~a~nviolates predicate~n ~a" (pretty v) (pretty c))]))
+  (define ce-prog
+    (cond [ce (format "An example program that breaks it:~n ~a" (pretty ce))]
+          [else ""]))
+  (error (format "Contract violation: ~a~n~a~n~a" parties reason ce-prog)))
 
 (: pretty : Any → String)
 (define (pretty x)
