e384fc5f7576e74cbd58670560d8ba3caf8fd464
Phil Nguyen <pcn@cs.umd.edu>
2014-12-17 15:57:18 -0500

    document example

--- examples/div.rkt
+++ examples/div.rkt
@@ -4,7 +4,5 @@
   (provide/contract
    [f ((integer? integer? . -> . integer?) integer? integer? integer? integer? . -> . real?)])
   (define (f g a b c d)
+    ;; If you change (g c d) to (g a b), the program is safe
     (/ 1 (- 10 (* (g a b) (g c d))))))
-
-#;(require 'div)
-#;((λ (x) (x (λ (x y) ((λ (x) 1) y)) 0 0 0 0)) f)
