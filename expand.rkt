a41affc3d0a55903040eb2bdc6bd5d8766294342
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-01-13 14:36:06 -0500

    Add expander file.

---
+++ expand.rkt
@@ -0,0 +1,96 @@
+#lang racket/base
+
+(require syntax/parse syntax/modresolve
+         (only-in racket/list append-map last-pair filter-map first add-between)
+         racket/path
+         racket/dict racket/match
+         racket/format)
+
+
+
+(define (identifier-binding* i)
+  (if (dict-ref lexical-bindings i #f)
+      'lexical
+      (identifier-binding i)))
+
+(define (hash-set* h . kvs)
+  (let loop ([kvs kvs] [h h])
+    (if (null? kvs)
+        h
+        (let* ([k (car kvs)]
+               [v (cadr kvs)]
+               [h (if v (hash-set h k v) h)])
+          (loop (cddr kvs) h)))))
+
+(define (hash* . kvs) (apply hash-set* (hash) kvs))
+
+
+(define lexical-bindings (make-free-id-table))
+
+(define (register-all! x)
+  (cond [(list? x) (for-each register-all! x)]
+        [(identifier? x) (dict-set! lexical-bindings x #t)]
+        [(pair? x)
+         (register-all! (car x))
+         (register-all! (cdr x))]
+        [(syntax? x) (register-all! (syntax-e x))]
+        [else (error 'register-all! "unexpected ~a" x)]))
+
+(define (do-expand stx in-path)
+  ;; error checking
+  (syntax-parse stx #:literals ()
+    [((~and mod-datum (~datum module)) n:id lang:expr . rest)
+     (void)]
+    [((~and mod-datum (~datum module)) . rest)
+     (error 'do-expand "got ill-formed module: ~a\n" (syntax->datum #'rest))]
+    [rest
+     (error 'do-expand "got something that isn't a module: ~a\n" (syntax->datum #'rest))])
+  ;; work
+  (parameterize ([current-namespace (make-base-namespace)])
+    (namespace-syntax-introduce (expand stx))))
+
+(define current-module (make-parameter #f))
+
+(define (index->path i)
+  (define-values (v _) (module-path-index-split i))
+  (if v
+      (list (resolved-module-path-name (module-path-index-resolve i)) #f)
+      (list (current-module) #t)))
+
+
+(define keep-srcloc (make-parameter #t))
+
+
+
+(require syntax/id-table)
+(define table (make-free-id-table))
+(define sym-table (make-hash))
+
+(define (gen-name id)
+  ;; use strings to make sure unreadablility isn't an issue
+  (if (hash-ref sym-table (symbol->string (syntax-e id)) #f)
+      (gensym (syntax-e id))
+      (begin (hash-set! sym-table (symbol->string (syntax-e id)) #t)
+             (syntax-e id))))
+
+(define quoted? (make-parameter #f))
+
+(define (id->sym id)
+  (define sym (identifier-binding-symbol id))
+  (define sym*
+    (if (eq? 'lexical (identifier-binding id))
+        (dict-ref! table id (λ _
+                               ;(printf ">>> miss: ~a\n" id)
+                               (gen-name id)))
+        sym))
+  ;(printf ">>> id sym sym*: ~a ~a ~a\n" id sym sym*)
+  (symbol->string
+   (if (quoted?)
+       (syntax-e id)
+       sym*)))
+
+
+;; example use
+
+(module+ main
+  (do-expand (read-syntax #f (open-input-string "(module m racket 5)")) #f))
