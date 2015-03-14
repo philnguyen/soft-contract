e2bcdc2319733db038cebf0f65db84a61057cd49
Phil Nguyen <pcn@cs.umd.edu>
2015-03-13 20:18:26 -0400

    fix diff script

--- soft-contract/benchmark-verification/dump-diff.rkt
+++ soft-contract/benchmark-verification/dump-diff.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require racket/system racket/file racket/format)
+(require racket/system racket/port racket/file racket/format)
 
 (for* ([sub-dir (in-list (directory-list "safe"))]
        [file (in-list (directory-list (format "safe/~a" sub-dir)))]
@@ -8,6 +8,10 @@
        #:when (regexp-match-exact? #rx".*.sch" fn)
        [fn-unsafe (in-value (format "fail-ce/~a/~a" sub-dir fn))]
        #:when (file-exists? fn-unsafe))
-  (printf "Diff for ~a:~n" fn)
-  (system (format "diff safe/~a/~a ~a" sub-dir fn fn-unsafe))
-  (printf "~n"))
+  (printf "=========================================~n")
+  (printf "Diff between safe/~a/~a and ~a:~n" sub-dir fn fn-unsafe)
+  (printf ; hack to play nice with piping
+   "~a~n"
+   (with-output-to-string
+     (λ ()
+       (system (format "diff safe/~a/~a ~a" sub-dir fn fn-unsafe))))))
