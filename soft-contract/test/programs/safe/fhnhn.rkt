11054ac9d88d2e0486e6ca96d3097b6ee8491ce9
Phil Nguyen <pcn@cs.umd.edu>
2015-04-07 11:23:07 -0400

    turn `fhnhn` to a single-module program

--- soft-contract/test/programs/safe/fhnhn.rkt
+++ soft-contract/test/programs/safe/fhnhn.rkt
@@ -3,15 +3,14 @@
 
 (define (h x) (λ (_) x))
 
-(define (g n) ((f (h n)) (h n)))
+(define (g f n) ((f (h n)) (h n)))
 
-(define (main m) (g m))
+(define (main f m) (g f m))
 
 (provide/contract
- [f (->i ([x (any/c . -> . integer?)])
+ [main ((->i ([x (any/c . -> . integer?)])
 	 (res (x)
 	      ((and/c (any/c . -> . integer?)
-		      (λ (y) (not (and (> (x #f) 0) (< (y #f) 0))))) . -> . integer?)))]
- [main (integer? . -> . integer?)]
- [h (integer? . -> . (any/c . -> . integer?))]
- [g (integer? . -> . integer?)])
+		      (λ (y) (not (and (> (x #f) 0) (< (y #f) 0))))) . -> . integer?)))
+        integer? . -> . integer?)]
+ [h (integer? . -> . (any/c . -> . integer?))])
