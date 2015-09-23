33ff89561ce69af40f020ed01006d2e69c98dc78
Phil Nguyen <pcn@cs.umd.edu>
2015-09-22 22:11:06 -0400

    prevent building up expression when reading from mutable state

--- soft-contract/runtime.rkt
+++ soft-contract/runtime.rkt
@@ -423,7 +423,9 @@
        [('not (list (-not e))) e]
        ; (car (cons e _)) = e
        [((-st-ac s i) (list (-@ (-st-mk s) es _)))
-        (list-ref es i)]
+        (cond ; don't build up syntax when reading from mutable states
+          [(∋ (-struct-info-mutables s) i) #f]
+          [else (list-ref es i)])]
        ; (cons (car e) (cdr e)) = e
        [((-st-mk s) es)
         (or (access-same-value? s es)
