821bf63853321af577edddebc58f6b44514cfcd7
Phil Nguyen <pcn@cs.umd.edu>
2015-11-15 17:57:57 -0500

    convert implications module to TR

--- soft-contract/prim-gen.rkt
+++ soft-contract/prim-gen.rkt
@@ -1,53 +1,54 @@
-#lang racket/base
+#lang typed/racket/base
 
-(require
- racket/match racket/contract racket/set "utils.rkt"
- (prefix-in prims: "prims.rkt"))
+(require racket/match racket/set "utils.rkt")
+(require/typed "prims.rkt"
+  [(implications prims:implications) (Listof Any)])
 
-(provide
- graph?
- (contract-out
-  [implications graph?]
-  [exclusions graph?]))
+(provide Graph implications exclusions)
 
-(define graph? (hash/c symbol? (set/c symbol?)))
+(define-type Graph (HashTable Symbol (Setof Symbol)))
 
+(: fix : (∀ (X) (X → X) X → X))
 (define (fix f x)
   (define x* (f x))
   (if (equal? x x*) x (fix f x*)))
 
 ;; Compute a graph's reflexive-transitive closure
-(define/contract (refl-trans m)
-  (graph? . -> . graph?)
+(: refl-trans : Graph → Graph)
+(define (refl-trans m)
 
   ;; Compute `m`'s reflexive closure
   (define m₀
-    (let ([refl (λ (m k) (hash-update m k (λ (vs) (set-add vs k)) ∅))])
-      (for*/fold ([m m])
+    (let ([refl
+           (λ ([m : Graph] [k : Symbol])
+             (hash-update m k (λ ([vs : (Setof Symbol)]) (set-add vs k)) →∅))])
+      (for*/fold ([m : Graph m])
                  ([(l rs) (in-hash m)]
                   [m (in-value (refl m l))]
                   [r (in-set rs)])
         (refl m r))))
 
+  (printf "graph:~n~a~n" m₀)
+
   ;; Compute `m`'s transitive closure
   (fix
-   (λ (m)
-     (for/hash ([(l rs) (in-hash m)])
+   (λ ([m : Graph])
+     (for/hash : Graph ([(l rs) (in-hash m)])
        (define rs*
-         (for/fold ([rs* rs]) ([r rs])
+         (for/fold ([rs* : (Setof Symbol) rs]) ([r rs])
            (∪ rs* (hash-ref m r))))
        (values l rs*)))
    m₀))
 
 ;; Add edge to graph
-(define/contract (add-edge m l r)
-  (graph? symbol? symbol? . -> . graph?)
-  (hash-update m l (λ (rs) (set-add rs r)) ∅))
+(: add-edge : Graph Symbol Symbol → Graph)
+(define (add-edge m l r)
+  (hash-update m l (λ ([rs : (Setof Symbol)]) (set-add rs r)) →∅))
 
 ;; Reverse a graph
-(define/contract (reverse-graph m)
-  (graph? . -> . graph?)
-  (for*/fold ([m (hash)])
+(: reverse-graph : Graph → Graph)
+(define (reverse-graph m)
+  (for*/fold ([m : Graph (hash)])
              ([(l rs) (in-hash m)] [r rs])
     (add-edge m r l)))
 
@@ -56,32 +57,33 @@
 
     ;; Compute first versions of graphs to start with
     (define-values (im ex)
-      (for/fold ([im (hash)] [ex (hash)])
+      (for/fold ([im : Graph (hash)] [ex : Graph (hash)])
                 ([dec (in-list prims:implications)])
         (match dec
-          [`(,l ⇒ ,r)
+          [`(,(? symbol? l) ⇒ ,(? symbol? r))
            (values (add-edge im l r) ex)]
-          [`(#:partition ,r (,ls ...))
+          [`(#:partition ,(? symbol? r) (,ls ...))
            (define im*
-             (for/fold ([im im]) ([l ls])
-               (add-edge im l r)))
+             (for/fold ([im : Graph im]) ([l ls])
+               (add-edge im (assert l symbol?) r)))
            (define ex*
-             (for*/fold ([ex ex]) ([l ls] [l* ls] #:unless (eq? l l*))
-               (add-edge ex l l*)))
+             (for*/fold ([ex : Graph ex]) ([l ls] [l* ls] #:unless (eq? l l*))
+               (add-edge ex (assert l symbol?) (assert l* symbol?))))
            (values im* ex*)]
           [`(#:exclusion ,ss ...)
            (define im* ; to make sure `im` graph covers every key
-             (for/fold ([im im]) ([s ss])
+             (for/fold ([im : Graph im]) ([s ss])
+               (assert s symbol?)
                (add-edge im s s)))
            (define ex*
-             (for*/fold ([ex ex]) ([s ss] [s* ss] #:unless (eq? s s*))
-               (add-edge ex s s*)))
+             (for*/fold ([ex : Graph ex]) ([s ss] [s* ss] #:unless (eq? s s*))
+               (add-edge ex (assert s symbol?) (assert s* symbol?))))
            (values im* ex*)])))
     
     (define im* (refl-trans im))
     (define im*⁻¹ (reverse-graph im*))
     (define ex* ; ⟦a ⇒ ¬b, c ⇒ b⟧ ⇒ a ⇒ ¬c
-      (for*/fold ([ex ex])
+      (for*/fold ([ex : Graph ex])
                  ([(l rs) (in-hash ex)]
                   [r (in-set rs)]
                   [r* (in-set (hash-ref im*⁻¹ r))])
