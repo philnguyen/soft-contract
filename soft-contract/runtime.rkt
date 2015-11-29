f2329381499f3e43e07ce9f73de7e5e3cf6b252e
Phil Nguyen <pcn@cs.umd.edu>
2015-11-29 16:43:29 -0500

    simplify arity operations using dedicated struct at meta-language; test cases

--- soft-contract/runtime.rkt
+++ soft-contract/runtime.rkt
@@ -434,6 +434,94 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Operations on procedure arities
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Representation of arity in the meta-language to make it easier to manipulate
+;; in proof rules.
+;; In the object language, it's just Racket's numbers and lists.
+(define-data -Arity
+  (subset: -Simple-Arity
+    Integer
+    (struct -Arity-At-Least [val : Integer]))
+  (Listof -Simple-Arity))
+
+(: -arity-includes? : -Arity -Arity → Boolean)
+;; Check if arity `ar₁` subsumes `ar₂`
+(define (-arity-includes? ar₁ ar₂)
+
+  (define (-arity-supports-number? [ar : -Arity] [n : Integer]) : Boolean
+    (match ar
+      [(? exact-integer? m) (= m n)]
+      [(-Arity-At-Least m) (<= m n)]
+      [(? list? ars) (for/or ([ari ars]) (-arity-supports-number? ari n))]))
+
+  (define (-arity-supports-at-least? [ar : -Arity] [n : Integer]) : Boolean
+    (match ar
+      [(? exact-integer?) #f]
+      [(-Arity-At-Least m) (<= m n)]
+      [(? list? ars)
+       (define min-at-least
+         (for/fold ([min-at-least : (Option Integer) #f])
+                   ([ari ars])
+           (match ari
+             [(? exact-integer?) min-at-least]
+             [(-Arity-At-Least m)
+              (cond [min-at-least (min min-at-least m)]
+                    [else m])])))
+       (and min-at-least
+            (for/and ([i (in-range n min-at-least)])
+              (-arity-supports-number? ar i)))]))
+  
+  (match ar₂
+    [(? exact-integer? n) (-arity-supports-number? ar₁ n)]
+    [(-Arity-At-Least n) (-arity-supports-at-least? ar₁ n)]
+    [(? list? ars₂) (for/and ([ari ars₂]) (-arity-includes? ar₁ ari))]))
+
+(: -procedure-arity : -V → (Option -Arity))
+(define (-procedure-arity V)
+
+  (define (-formals-arity [xs : -formals]) : -Arity
+    (cond
+      [(-varargs? xs) (-Arity-At-Least (length (-varargs-init xs)))]
+      [else (length xs)]))
+
+  (match V
+    [(or (-Clo xs _ _ _) (-Clo* xs _ _)) (-formals-arity (assert xs))]
+    [(or (-And/C #t _ _) (-Or/C #t _ _) (? -Not/C?)) 1]
+    [(-=>i xs _ _ _ _ _) (length xs)]
+    [(-●) #f]
+    [_ (error '-procedure-arity "called on a non-procedure ~a" (show-V V))]))
+
+(module+ test
+  (require typed/rackunit)
+  (check-true (-arity-includes? 1 1) "1-1")
+  (check-true (-arity-includes? (list 1) 1) "(1)-1")
+  (check-true (-arity-includes? 1 (list 1)) "1-(1)")
+  (check-false (-arity-includes? 1 (-Arity-At-Least 1)) "1-≥1")
+  (check-true (-arity-includes? (-Arity-At-Least 1) 1) "≥1-1")
+  (check-true (-arity-includes? (-Arity-At-Least 1) (list 1 (-Arity-At-Least 2))) "≥1-1")
+  (check-true (-arity-includes? (list 1 (-Arity-At-Least 2)) (-Arity-At-Least 1))
+              "(1,≥2)-≥1")
+  (check-true (-arity-includes? (-Arity-At-Least 1) (list 1 (-Arity-At-Least 3)))
+              "≥1-(1,≥3)")
+  (check-false (-arity-includes? (list 1 (-Arity-At-Least 3)) (-Arity-At-Least 1))
+               "(1,≥3)-≥1")
+  (check-true (-arity-includes? (list 0 1 2 (-Arity-At-Least 3))
+                                (list (-Arity-At-Least 0)))
+              "(0,1,2,≥3)-≥0")
+  (check-true (-arity-includes? (list (-Arity-At-Least 0))
+                                (list 0 1 2 (-Arity-At-Least 3)))
+              "(≥0)-(0,1,2,≥3)")
+  (check-false (-arity-includes? (list 0 2 (-Arity-At-Least 3))
+                                 (list (-Arity-At-Least 0)))
+               "(0,2,≥3)-(≥0)")
+  (check-true (-arity-includes? (list (-Arity-At-Least 0))
+                                (list 0 2 (-Arity-At-Least 3)))
+              "(≥0)-(0,2,≥3)"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; Convenience
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -608,7 +696,7 @@
     [_ (for/list : (Listof -?e) ([i (in-range (-struct-info-arity s))])
          (-?@ (-st-ac s i) e))]))
 
-(: -app-split : -?e Symbol Integer → (Listof -?e))
+(: -app-split : -?e -o Integer → (Listof -?e))
 (define (-app-split e o n)
   (match e
     [(-@ (≡ o) es _) es]
