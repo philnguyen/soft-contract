b56c0040e52e6de79329bc0349b3c2b2be9b1943
Phil Nguyen <pcn@cs.umd.edu>
2015-12-05 13:34:57 -0500

    rename

--- soft-contract/utils/main.rkt
+++ soft-contract/utils/main.rkt
@@ -1,6 +1,6 @@
 #lang typed/racket/base
 (require "debug.rkt" "def.rkt" "eval.rkt" "function.rkt" "list.rkt"
-         "multi-map.rkt" "non-determinism.rkt" "pretty.rkt" "set.rkt" "sexp-stx.rkt")
+         "map.rkt" "non-determinism.rkt" "pretty.rkt" "set.rkt" "sexp-stx.rkt")
 (provide
  (all-from-out "debug.rkt" "def.rkt" "eval.rkt" "function.rkt" "list.rkt"
-               "multi-map.rkt" "non-determinism.rkt" "pretty.rkt" "set.rkt" "sexp-stx.rkt"))
+               "map.rkt" "non-determinism.rkt" "pretty.rkt" "set.rkt" "sexp-stx.rkt"))
