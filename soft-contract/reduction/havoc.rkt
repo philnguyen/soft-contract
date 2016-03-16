bea76e65d10684ec166a0a0a7f1a1a7ed01cd033
Phil Nguyen <pcn@cs.umd.edu>
2016-03-16 15:32:16 -0400

    simplify havoc

--- soft-contract/reduction/havoc.rkt
+++ soft-contract/reduction/havoc.rkt
@@ -1,6 +1,6 @@
 #lang typed/racket/base
 
-(provide gen-havoc-Clo gen-havoc-exp havoc-id havoc-src)
+(provide gen-havoc-Clo gen-havoc-exp havoc-id)
 
 (require
  racket/match racket/set
@@ -19,7 +19,7 @@
   
   ;; Collect all defined accessors (`defs`) and exported identifiers (`decs`)
   (define defs : (HashTable Symbol -st-ac) (make-hasheq))
-  (define decs : (HashTable Symbol #t) (make-hasheq))
+  (define decs : (HashTable Symbol #t    ) (make-hasheq))
   (for* ([m ms]
          [form (-module-body m)])
     (match form
@@ -27,9 +27,8 @@
        (for-each
         (match-lambda [(-p/c-item x _) (hash-set! decs x #t)])
         specs)]
-      [(-define-values _ (list x) e)
-       (when (-st-ac? e)
-         (hash-set! defs x e))]
+      [(-define-values _ (list x) (? -st-ac? e))
+       (hash-set! defs x e)]
       [_ (void)]))
   
   ;; Return exported accessors
@@ -37,18 +36,22 @@
     ac))
 
 (: gen-havoc-Clo : (Listof -module) → -Clo)
-; Only used by `verify` module, not `ce`
+;; Generate the unknown context
+;; Only used by `verify` module, not `ce`
 (define (gen-havoc-Clo ms)
 
   (define (havoc-ref-from [ctx : Mon-Party] [pos : Integer])
     (-ref havoc-id ctx pos))
 
   (define x (-x '☠))
+
   (define acs-for-struct
-    (for/fold ([m : (HashTable -struct-info (Setof -st-ac)) (hash)])
+    (for/fold ([m : (HashTable -struct-info (℘ -st-ac)) (hash)])
               ([ac (prog-accs ms)])
       (match-define (-st-ac si _) ac)
-      (hash-update m si (λ ([acs : (Setof -st-ac)]) (set-add acs ac)) (λ () {set ac}))))
+      (hash-update m si (λ ([acs : (℘ -st-ac)]) (set-add acs ac))
+                        (λ () {set ac}))))
+  
   (define alts
     (cons
      (cons (-@ 'procedure? (list x) havoc-src)
@@ -61,51 +64,23 @@
                 (-@ (havoc-ref-from havoc-path (next-loc!))
                     (list (-@ ac (list x) havoc-src))
                     havoc-src)))))))
+  
   (define havoc-body (-cond alts (-amb ∅)))
   
-  (-Clo '(☠) (⇓ havoc-body) ⊥ρ))
+  (-Clo '(☠) (⇓ havoc-body) ⊥ρ ⊤Γ))
 
 (: gen-havoc-exp : (Listof -module) → -e)
+;; Generate havoc top-level expression havoc-king modules' exports
 (define (gen-havoc-exp ms)
-
-  ;; Generate havoc-ing expression
   (define-set refs : -ref)
-  ;(log-debug "~nmodules: ~n~a~n" ms)
+  
   (for ([m (in-list ms)])
-    (cond
-      [(module-opaque? m)
-       =>
-       (λ (s)
-         (log-debug "Omit havocking opaque module `~a`. Provided but undefined: ~a~n"
-                    (-module-path m)
-                    (set->list s)))]
-      [else
-       #;(log-debug "Havocking transparent module ~a~n" (-module-path m))
-       (match-define (-module path forms) m)
-       #;(eprintf "Insert exported identifiers from module ~a to unknown contexts~n" path)
-       (for* ([form (in-list forms)] #:when (-provide? form)
-              [spec (in-list (-provide-specs form))])
-         (log-debug "adding: ~a~n" (-p/c-item-id spec))
-         (refs-add! (-ref (-id (-p/c-item-id spec) path) '† (next-loc!))))]))
-  ;(log-debug "~nrefs: ~a~n" refs)
+    (match-define (-module path forms) m)
+    (for* ([form forms] #:when (-provide? form)
+           [spec (-provide-specs form)])
+      (match-define (-p/c-item x _) spec)
+      (refs-add! (-ref (-id x path) '† (next-loc!)))))
+  
   (-amb/remember (for/list ([ref (in-set refs)])
                    (-@ (•!) (list ref) havoc-src))))
 
-(: module-opaque? : -module → (U #f (Setof Symbol)))
-;; Check whether module is opaque, returning the set of opaque exports if so
-(define (module-opaque? m)
-  (match-define (-module p body) m)
-  (case p
-    [(Λ † havoc) #|HACK|# ∅]
-    [else
-     (define-values (exports defines)
-       (for/fold ([exports : (Setof Symbol) ∅] [defines : (Setof Symbol) ∅])
-                 ([e (in-list body)])
-         (match e
-           [(-provide _ specs)
-            (values (set-add-list exports (map -p/c-item-id specs)) defines)]
-           [(-define-values _ xs _)
-            (values exports (set-add-list defines xs))]
-           [_ (values exports defines)])))
-
-     (if (⊆ exports defines) #f (-- exports defines))]))
