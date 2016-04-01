1850c48404c635f67697bbf9790627826e542d7b
Phil Nguyen <pcn@cs.umd.edu>
2016-04-01 17:45:46 -0400

    add `match` test

---
+++ soft-contract/test/programs/safe/match.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require soft-contract/fake-contract)
+
+(define f
+  (match-lambda
+    [(cons x y) #:when (> x 4) (add1 x)]
+    [(? number? z) (add1 z)]
+    [(? string? s) (string-length s)]))
+
+(provide
+ (contract-out
+  [f ((or/c (or/c string? number?) (cons/c (>=/c 5) any/c)) . -> . any/c)]))
