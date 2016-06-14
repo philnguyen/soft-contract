7111e9250d23ce2e156a8a95a571c11ad0ec3a2a
Phil Nguyen <pcn@cs.umd.edu>
2016-06-14 14:30:20 -0400

    debuggings

--- soft-contract/reduction/main.rkt
+++ soft-contract/reduction/main.rkt
@@ -159,13 +159,16 @@
              (for ([(y i) (in-indexed ys)])
                (printf "      ↦~a~a ~a~n" (n-sup (add1 i)) (n-sub n) (show-y y)))))
 
-         ;((inst show-m -α -V) 'δσ show-α show-V δσ #:filter (λ (α) (not (or (-α.def? α) (-α.wrp? α) (-e? α)))))
-         ;((inst show-m -τ -A) 'δM show-τ show-A δM)
-         ;((inst show-m -τ -ℛ) 'δΞ show-τ show-ℛ δΞ)
+         #;(begin ; print global table deltas
+           ((inst show-m -α -V) 'δσ show-α show-V δσ #:filter (λ (α) (not (or (-α.def? α) (-α.wrp? α) (-e? α)))))
+           ((inst show-m -τ -A) 'δM show-τ show-A δM)
+           ((inst show-m -τ -ℛ) 'δΞ show-τ show-ℛ δΞ))
+         
          (let* ([now (current-seconds)]
                 [δ (- now last)])
            (set! last now)
            (printf "time: ~as~n" δ))
+         
          #;(match (read) ; interactive
            ['done (error "done")]
            [(? exact-nonnegative-integer? i)
