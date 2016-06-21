62d31cda24da998de7ae6846dbe41fbf99e7ae48
Phil Nguyen <pcn@cs.umd.edu>
2016-06-21 15:38:09 -0400

    kill threads when timeout

--- soft-contract/utils/eval.rkt
+++ soft-contract/utils/eval.rkt
@@ -6,8 +6,10 @@
 ;; return singleton list of value, or #f on timeout
 (define-syntax-rule (within-time: τ n e ...)
   (let ([c : (Channelof (U #f (List τ))) (make-channel)])
-    (define t₁ (thread (λ () (channel-put c (list (begin e ...))))))
+    (define t₁ (thread (λ () (with-handlers ([exn? (λ (_) (channel-put c #f))])
+                               (channel-put c (list (begin e ...)))))))
     (define t₂ (thread (λ () (sleep n) (channel-put c #f))))
+    (define (kill-all) (kill-thread t₁) (kill-thread t₂))
     (cond
-      [(channel-get c) => (λ ([ans : (List τ)]) (kill-thread t₂) ans)]
-      [else (kill-thread t₁) #f])))
+      [(channel-get c) => (λ ([ans : (List τ)]) (kill-all) ans)]
+      [else (kill-all) #f])))
