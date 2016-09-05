f4628376364d54d965bec39533e1aada9295f510
Phil Nguyen <pcn@cs.umd.edu>
2016-09-05 16:44:27 -0400

    dependencies

--- soft-contract/info.rkt
+++ soft-contract/info.rkt
@@ -2,5 +2,10 @@
 
 (define collection "soft-contract")
 
+(define deps '("base"
+               "typed-racket-lib"
+               "typed-racket-more"
+               "z3"))
+
 (define compile-omit-paths '("test"))
 
