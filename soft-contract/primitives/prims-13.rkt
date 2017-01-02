a72ef398d0152a99aa8fbb73c4d513a4d82c3d1a
Phil Nguyen <pcn@cs.umd.edu>
2017-01-02 16:51:52 -0500

    make sure some lifting are disabled

--- soft-contract/primitives/prims-13.rkt
+++ soft-contract/primitives/prims-13.rkt
@@ -14,12 +14,12 @@
 (def-prim output-port? (any/c . -> . boolean?))
 (def-prim port? (any/c . -> . boolean?))
 (def-prim eof-object? (any/c . -> . boolean?))
-(def-prim current-input-port  (-> input-port?))
-(def-prim current-output-port (-> output-port?))
-(def-prim current-error-port (-> output-port?))
+(def-prim current-input-port  (-> input-port?) #:lift-concrete? #f)
+(def-prim current-output-port (-> output-port?) #:lift-concrete? #f)
+(def-prim current-error-port (-> output-port?) #:lift-concrete? #f)
 
 ;; 13.1.3 Port Buffers and Positions
-(def-prim flush-output (-> void?)) ; FIXME uses
+(def-prim flush-output (-> void?) #:lift-concrete? #f) ; FIXME uses
 
 ;; 13.1.5 File Ports
 ; [HO] call-with-input-file call-with-output-file
@@ -28,18 +28,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; 13.2 Byte and String Input
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def-prim read-char (input-port? . -> . (or/c char? eof-object?))) ; FIXME uses
-(def-prim peek-char (input-port? . -> . (or/c char? eof-object?))) ; FIXME uses
+(def-prim read-char (input-port? . -> . (or/c char? eof-object?)) #:lift-concrete? #f) ; FIXME uses
+(def-prim peek-char (input-port? . -> . (or/c char? eof-object?)) #:lift-concrete? #f) ; FIXME uses
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; 13.3 Byte and String Output
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def-prim write-char (char? output-port? . -> . void?)) ; FIXME uses
-(def-prim newline (output-port? . -> . void?)) ; FIXME uses
+(def-prim write-char (char? output-port? . -> . void?) #:lift-concrete? #f) ; FIXME uses
+(def-prim newline (output-port? . -> . void?) #:lift-concrete? #f) ; FIXME uses
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; 13.5 Writing
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def-prim write (any/c . -> . void?)) ; FIXME uses
-(def-prim display (any/c output-port? . -> . void?)) ; FIXME uses
+(def-prim write (any/c output-port? . -> . void?) #:lift-concrete? #f) ; FIXME uses
+(def-prim display (any/c output-port? . -> . void?) #:lift-concrete? #f) ; FIXME uses
