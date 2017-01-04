fe07a63f7d1c3d16aab1be4beab069f139d2c68b
Phil Nguyen <pcn@cs.umd.edu>
2017-01-04 13:26:11 -0500

    fix exports

--- soft-contract/externals/main.rkt
+++ soft-contract/externals/main.rkt
@@ -1,5 +1,5 @@
 #lang typed/racket/base
 
-(require "exts.rkt")
+(require "def-ext-runtime.rkt" "exts.rkt")
 
-(provide (all-from-out "exts.rkt"))
+(provide (all-from-out "def-ext-runtime.rkt" "exts.rkt"))
