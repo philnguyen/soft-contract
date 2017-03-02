ac3ea0ce2566466b444b845df751b067fa4ec5d4
Phil Nguyen <pcn@cs.umd.edu>
2017-03-02 15:20:07 -0500

    some missed ones

---
+++ soft-contract/test/programs/safe/mochi/intro1.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(define (f x g) (g (+ x 1)))
+(define (main n h)
+  (if (> n 0) (f n h) 'unit))
+
+(provide
+ (contract-out
+  [main (integer? ((and/c integer? (>/c 0)) . -> . any/c) . -> . any/c)]))
