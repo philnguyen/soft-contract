1690b9af3aa46403e70595345753234f7fde1f93
Phil Nguyen <pcn@cs.umd.edu>
2017-07-19 16:14:04 -0400

    update test suite against more precise implementation

--- soft-contract/test/programs/paper/escape-safe.rkt
+++ soft-contract/test/programs/paper/escape-safe.rkt
@@ -7,7 +7,7 @@
   (define inc! (λ () (set! n (+ 1 n))))
   (if (= n 0) 1 (/ 1 n)) ; 1/n unreachable
   (g inc!)
-  (if (= n 0) 1 (begin (g (λ () (void))) (/ 2 n))) ; 2/n unreachable despite `inc!` escaped
+  (if (= n 0) 1 (/ 2 n)) ; 2/n unreachable despite `inc!` escaped
   (define m n)
   (if (= m 0) 1 (/ 3 m)) ; 3/m unreachable
   )
