6d691c3877eab890cb30c397e78386f9310a39b2
Phil Nguyen <pcn@cs.umd.edu>
2017-07-21 11:44:35 -0400

    fix test to match one in paper

--- soft-contract/test/programs/paper/escape-conservative.rkt
+++ soft-contract/test/programs/paper/escape-conservative.rkt
@@ -1,9 +1,12 @@
 #lang racket
 
 (provide/contract
- [f ((any/c . -> . any) . -> . number?)])
+ [f (((-> void?) . -> . void?) . -> . (and/c (<=/c 2) integer?))])
 
 (define (f g)
   (define n 0)
-  (g (λ () (set! n 'nope)))
-  n)
+  (define inc! (λ () (set! n (+ 1 n))))
+  (g inc!)
+  (if (< n 2)
+      (begin (g (λ () (void))) n)
+      2))
