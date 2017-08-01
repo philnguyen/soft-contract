05c7505fc1d37f38dbcb4d976aab0b9ff704b173
Phil Nguyen <pcn@cs.umd.edu>
2017-08-01 00:19:16 -0400

    undo brittle hack compacting cache

--- soft-contract/proof-relation/widen.rkt
+++ soft-contract/proof-relation/widen.rkt
@@ -411,9 +411,7 @@
       (define tₓ* (if looped? (-x x) (or tₓ (-x x))))
       (define α (-α->⟪α⟫ (-α.x x ⟪ℋ⟫)))
       (σ⊕V! Σ α Vₓ*)
-      (define $*
-        (let ([Vs (σ@ Σ α)])
-          (if (> (set-count Vs) 1) $ ($-set $ x (-W¹ Vₓ* tₓ*)))))
+      (define $* ($-set $ x (-W¹ Vₓ* tₓ*)))
       (values (ρ+ ρ x α) $*)))
 
   (: alloc-rest-args! ([-Σ -Γ -⟪ℋ⟫ ℓ (Listof -W¹)] [#:end -V] . ->* . -V))
