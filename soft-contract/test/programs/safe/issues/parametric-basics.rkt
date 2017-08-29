5487f1f3cac08649d2105cf229971ed0e7b9a2d4
Phil Nguyen <pcn@cs.umd.edu>
2017-08-29 12:44:25 -0400

    more tests for parametric contracts

--- soft-contract/test/programs/safe/issues/parametric-basics.rkt
+++ soft-contract/test/programs/safe/issues/parametric-basics.rkt
@@ -8,6 +8,29 @@
 (define (use-compose compose)
   ((compose add1 add1) 42))
 
+(define (map f xs)
+  (match xs
+    ['() '()]
+    [(cons x xs*) (cons (f x) (map f xs*))]))
+
+(define (foldr f a xs)
+  (match xs
+    ['() a]
+    [(cons x xs*) (f x (foldr f a xs*))]))
+
+(define (foldl f a xs)
+  (match xs
+    ['() a]
+    [(cons x xs*) (foldl f (f x a) xs*)]))
+
+(define ((map-from-foldr foldr) f xs)
+  (foldr (λ (x xs*)
+           (cons (f x) xs*))
+         '()
+         xs))
+
+(define ((compose f g) x) (f (g x)))
+
 (provide
  (contract-out
   [id (parametric->/c (α) (α . -> . α))]
@@ -18,4 +41,10 @@
   [use-compose
    ((parametric->/c (α β γ) ((β . -> . γ) (α . -> . β) . -> . (α . -> . γ)))
     . -> .
-    exact-positive-integer?)]))
+    exact-positive-integer?)]
+  [map (parametric->/c (α β) ((α . -> . β) (listof α) . -> . (listof β)))]
+  [foldr (parametric->/c (α β) ((α β . -> . β) β (listof α) . -> . β))]
+  [foldl (parametric->/c (α β) ((α β . -> . β) β (listof α) . -> . β))]
+  [map-from-foldr ((parametric->/c (α β) ((α β . -> . β) β (listof α) . -> . β))
+                   . -> .
+                   (parametric->/c (α β) ((α . -> . β) (listof α) . -> . (listof β))))]))
