a3a1b3acb942b53497f14d725fb32ae825842f8c
Phil Nguyen <pcn@cs.umd.edu>
2017-10-05 23:51:29 -0400

    add fector to sanity-test suite

--- soft-contract/test/sanity-test.rkt
+++ soft-contract/test/sanity-test.rkt
@@ -191,6 +191,7 @@
   (test "unsafe/issues/strict-parametricity.rkt" check-fail)
 
   (test "safe/real/hash-srfi-69.rkt" (check 'Ok-pos 1 1))
+  (test "safe/real/fector.rkt" (check 'Ok-pos 4 6)) ; depends count how
 
   (test   "safe/real/protected-leftist-tree.rkt" check-safe)
   (test "unsafe/real/protected-leftist-tree.rkt" check-fail)
