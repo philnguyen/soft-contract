db77744fe8344c051dd64b2419ab0951530bb8c5
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2017-11-30 16:07:00 -0500

    Add info file.

---
+++ info.rkt
@@ -0,0 +1,16 @@
+#lang info
+(define collection 'multi)
+(define deps '("base"
+               "bnf"
+               "compiler-lib"
+               "htdp-lib"
+               "intern"
+               "macro-debugger-text-lib"
+               "math-lib"
+               "profile-lib"
+               "rackunit-typed"
+               "set-extras"
+               "typed-racket-lib"
+               "web-server-lib"
+               "z3"
+               "zo-lib"))
