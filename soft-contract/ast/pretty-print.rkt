a7d5c86326b5b06891e3c05ce812d2a9572d932a
Phil Nguyen <pcn@cs.umd.edu>
2019-04-01 15:39:24 +0000

    tweak pretty printing

--- soft-contract/ast/pretty-print.rkt
+++ soft-contract/ast/pretty-print.rkt
@@ -38,9 +38,9 @@
   (define show-o : (-o → Symbol)
     (match-lambda
       [(? symbol? s) s]
-      [(-st-mk 𝒾) (format-symbol "struct:~a" (-𝒾-name 𝒾))]
-      [(-st-ac 𝒾 i) (format-symbol "struct:~a" (show-ac 𝒾 i))]
-      [(-st-p 𝒾) (format-symbol "struct:~a?" (-𝒾-name 𝒾))]
+      [(-st-mk 𝒾) (format-symbol "_~a" (-𝒾-name 𝒾))]
+      [(-st-ac 𝒾 i) (format-symbol "_~a" (show-ac 𝒾 i))]
+      [(-st-p 𝒾) (format-symbol "_~a?" (-𝒾-name 𝒾))]
       [(-st-mut (== -𝒾-mcons) 0) 'set-mcar!]
       [(-st-mut (== -𝒾-mcons) 1) 'set-mcdr!]
       [(-st-mut (== -𝒾-box) _) 'set-box!]
